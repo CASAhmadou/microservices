921f2b9089b443cdee934d4541a84425ab935e60
CASAhmadou <amalsakho92@gmail.com>
2023-07-19 18:35:13 +0000

    Common

---
+++ second-service/src/main/java/com/ecole221/l3devweb/second/service/controller/PersonneController.java
@@ -0,0 +1,4 @@
+package com.ecole221.l3devweb.second.service.controller;
+
+public class PersonneController {
+}
---
+++ second-service/src/main/java/com/ecole221/l3devweb/second/service/service/Utils.java
@@ -0,0 +1,4 @@
+package com.ecole221.l3devweb.second.service.service;
+
+public class Utils {
+}
